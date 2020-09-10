bfa509262696db21074d33ea73507449587c8a52
DanielFBaran <danielbaranalt@gmail.com>
2020-09-10 20:16:02 -0300

    Alteracoes Testes

--- src/main/java/com/cartoes/api/entities/Cliente.java
+++ src/main/java/com/cartoes/api/entities/Cliente.java
@@ -1,4 +1,5 @@
 package com.cartoes.api.entities;
+
 import java.io.Serializable;
 import java.util.Date;
 import java.util.List;
@@ -14,67 +15,86 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 @Entity
 @Table(name = "cliente")
 public class Cliente implements Serializable {
- private static final long serialVersionUID = 1L;
- @Id
- @GeneratedValue(strategy = GenerationType.AUTO)
- private int id;
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
 
- @Column(name = "nome", nullable = false, length = 100)
- private String nome;
+	@Column(name = "nome", nullable = false, length = 100)
+	private String nome;
 
- @Column(name = "cpf", nullable = false, length = 11, unique = true)
- private String cpf;
+	@Column(name = "cpf", nullable = false, length = 11, unique = true)
+	private String cpf;
 
- @Column(name = "uf", nullable = false, length = 2)
- private String uf;
+	@Column(name = "uf", nullable = false, length = 2)
+	private String uf;
 
- @Column(name = "data_Atualizacao", nullable = false)
- private Date dataAtualizacao;
+	@Column(name = "data_Atualizacao", nullable = false)
+	private Date dataAtualizacao;
 
- @JsonManagedReference
- @OneToMany(mappedBy = "cliente", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
- private List<Cartao> cartoes;
- 
- public int getId() {
- return id;
- }
+	@JsonManagedReference
+	@OneToMany(mappedBy = "cliente", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	private List<Cartao> cartoes;
 
- public void setId(int id) {
- this.id = id;
- }
- public String getNome() {
- return nome;
- }
+	public int getId() {
+		return id;
+	}
 
- public void setNome(String nome) {
- this.nome = nome;
- }
+	public void setId(int id) {
+		this.id = id;
+	}
 
- public String getCpf() {
- return cpf;
- }
+	public String getNome() {
+		return nome;
+	}
 
- public void setCpf(String cpf) {
- this.cpf = cpf;
- }
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
- public String getUf() {
- return uf;
- }
+	public Date getDataAtualizacao() {
+		return dataAtualizacao;
+	}
 
- public void setUf(String uf) {
- this.uf = uf;
- }
- 
- @PreUpdate
- public void preUpdate() {
-	 dataAtualizacao = new Date();
-	 }
-	 @PrePersist
-	 public void prePersist() {
-	 dataAtualizacao = new Date();
-	 }
+	public void setDataAtualizacao(Date dataAtualizacao) {
+		this.dataAtualizacao = dataAtualizacao;
+	}
+
+	public List<Cartao> getCartoes() {
+		return cartoes;
+	}
+
+	public void setCartoes(List<Cartao> cartoes) {
+		this.cartoes = cartoes;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getUf() {
+		return uf;
+	}
+
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
+
+	@PreUpdate
+	public void preUpdate() {
+		dataAtualizacao = new Date();
+	}
+
+	@PrePersist
+	public void prePersist() {
+		dataAtualizacao = new Date();
+	}
 }
