748666746c2cf286da5702d25685bf0a8b27e048
DanielFBaran <danielbaranalt@gmail.com>
2020-09-12 10:26:40 -0300

    cartApp 12/08/2020

--- src/test/java/com/cartoes/api/CartoesApplicationTests.java
+++ src/test/java/com/cartoes/api/CartoesApplicationTests.java
@@ -1,18 +1,18 @@
-package com.cartoes.api;
+//package com.cartoes.api;
 
-import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.boot.test.context.SpringBootTest;
+//import org.junit.jupiter.api.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.test.context.ActiveProfiles;
+//import org.springframework.test.context.junit4.SpringRunner;
+//import org.springframework.boot.test.context.SpringBootTest;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@ActiveProfiles("test")
-class CartoesApplicationTests {
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//@ActiveProfiles("test")
+//class CartoesApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
-}
+//}
